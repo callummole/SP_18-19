b9e7c30c7faf6f73c05364d05f029ecbd9e77353
callummole <c.d.mole@leeds.ac.uk>
2019-01-29 10:33:33 +0000

    create midline only trial for recording 'automated' trajectories

---
+++ Orca19_midlineOnly.py
@@ -0,0 +1,365 @@
+"""
+Script to run silent failure paradigm, with cognitive load. Every trial begins with a period of automation. 
+
+The Class myExperiment handles execution of the experiment.
+
+This script relies on the following modules:
+
+For eyetracking - eyetrike_calibration_standard.py; eyetrike_accuracy_standard.py; also the drivinglab_pupil plugin.
+
+For perspective correct rendering - myCave.py
+
+For motion through the virtual world - vizdriver.py
+
+"""
+import sys
+
+rootpath = 'C:\\VENLAB data\\shared_modules\\Logitech_force_feedback'
+sys.path.append(rootpath)
+rootpath = 'C:\\VENLAB data\\shared_modules'
+sys.path.append(rootpath)
+rootpath = 'C:\\VENLAB data\\shared_modules\\pupil\\capture_settings\\plugins\\drivinglab_pupil\\'
+sys.path.append(rootpath)
+
+import viz # vizard library
+import numpy as np # numpy library - such as matrix calculation
+import random # python library
+import vizdriver_Orca18 as vizdriver # vizard library
+import viztask # vizard library
+import math as mt # python library
+import vizshape
+import vizact
+import vizmat
+import myCave
+import pandas as pd
+from TrackMaker import Bend
+#import PPinput
+
+def LoadEyetrackingModules():
+
+	"""load eyetracking modules and check connection"""
+
+	from eyetrike_calibration_standard import Markers, run_calibration
+	from eyetrike_accuracy_standard import run_accuracy
+	from UDP_comms import pupil_comms
+
+	###Connect over network to eyetrike and check the connection
+	comms = pupil_comms() #Initiate a communication with eyetrike	
+	#Check the connection is live
+	connected = comms.check_connection()
+
+	if not connected:
+		print("Cannot connect to Eyetrike. Check network")
+		raise Exception("Could not connect to Eyetrike")
+	else:
+		pass	
+	#markers = Markers() #this now gets added during run_calibration				
+	
+def LoadCave():
+	"""loads myCave and returns Caveview"""
+
+	#set EH in myCave
+	cave = myCave.initCave()
+	caveview = cave.getCaveView()
+	return (caveview)
+
+def GenerateConditionLists(FACTOR_radiiPool, FACTOR_YawRate_offsets, TrialsPerCondition):
+	"""Based on two factor lists and TrialsPerCondition, create a factorial design and return trialarray and condition lists"""
+
+	NCndts = len(FACTOR_radiiPool) * len(FACTOR_YawRate_offsets)	
+#	ConditionList = range(NCndts) 
+
+	#automatically generate factor lists so you can adjust levels using the FACTOR variables
+	ConditionList_radii = np.repeat(FACTOR_radiiPool, len(FACTOR_YawRate_offsets)	)
+	ConditionList_YawRate_offsets = np.tile(FACTOR_YawRate_offsets, len(FACTOR_radiiPool)	)
+
+	print (ConditionList_radii)
+	print (ConditionList_YawRate_offsets)
+
+	TotalN = NCndts * TrialsPerCondition
+
+	TRIALSEQ = range(0,NCndts)*TrialsPerCondition
+	np.random.shuffle(TRIALSEQ)
+
+	direc = [1,-1]*(TotalN/2) #makes half left and half right.
+	np.random.shuffle(direc) 
+
+	TRIALSEQ_signed = np.array(direc)*np.array(TRIALSEQ)
+
+	return (TRIALSEQ_signed, ConditionList_radii, ConditionList_YawRate_offsets)
+
+# ground texture setting
+def setStage():
+	
+	"""Creates grass textured groundplane"""
+	
+	
+	###should set this hope so it builds new tiles if you are reaching the boundary.
+	fName = 'C:/VENLAB data/shared_modules/textures/strong_edge.bmp'
+	
+	# add groundplane (wrap mode)
+	groundtexture = viz.addTexture(fName)
+	groundtexture.wrap(viz.WRAP_T, viz.REPEAT)	
+	groundtexture.wrap(viz.WRAP_S, viz.REPEAT)	
+	
+	groundplane = viz.addTexQuad() ##ground for right bends (tight)
+	tilesize = 500
+	planesize = tilesize/5
+	groundplane.setScale(tilesize, tilesize, tilesize)
+	groundplane.setEuler((0, 90, 0),viz.REL_LOCAL)
+	#groundplane.setPosition((0,0,1000),viz.REL_LOCAL) #move forward 1km so don't need to render as much.
+	matrix = vizmat.Transform()
+	matrix.setScale( planesize, planesize, planesize )
+	groundplane.texmat( matrix )
+	groundplane.texture(groundtexture)
+	groundplane.visible(1)	
+	
+	viz.clearcolor(viz.SKYBLUE)
+	
+	return groundplane
+	
+def BendMaker(radlist):
+	
+	"""makes left and right roads  for for a given radii and return them in a list"""
+	
+	leftbendlist = []
+	rightbendlist = []
+	grey = [.8,.8,.8]	
+
+	for r in radlist:
+		rightbend = Bend(startpos = [0,0], rads = r, x_dir = 1, colour = viz.RED, road_width=0)
+			
+		rightbendlist.append(rightbend)
+
+		leftbend = Bend(startpos = [0,0], rads = r, x_dir = -1, colour = viz.BLUE, road_width=0)
+		
+			
+		leftbendlist.append(leftbend)
+	
+	return leftbendlist,rightbendlist 
+
+class myExperiment(viz.EventClass):
+
+	def __init__(self, eyetracking, practice, exp_id, ppid = 1):
+
+		viz.EventClass.__init__(self)
+	
+		self.EYETRACKING = eyetracking
+		self.PRACTICE = practice		
+		self.EXP_ID = exp_id
+
+		if EYETRACKING == False:	
+			LoadEyetrackingModules()
+
+		self.PP_id = ppid
+		self.TrialLength = 15 #length of time that road is visible. Constant throughout experiment
+	
+		#### PERSPECTIVE CORRECT ######
+		self.caveview = LoadCave() #this module includes viz.go()
+
+		# #BirdsEye
+		# self.caveview.setPosition([0,100,0])
+		# self.caveview.setEuler([0,90,0])
+
+
+		##### SET CONDITION VALUES #####
+		self.FACTOR_radiiPool = [40] # A sharp and gradual bend
+		self.FACTOR_YawRate_offsets = [0] #6 yawrate offsets, specified in degrees per second.
+		self.TrialsPerCondition = 6
+		[trialsequence_signed, cl_radii, cl_yawrates]  = GenerateConditionLists(self.FACTOR_radiiPool, self.FACTOR_YawRate_offsets, self.TrialsPerCondition)
+
+		self.TRIALSEQ_signed = trialsequence_signed #list of trialtypes in a randomised order. -ve = leftwards, +ve = rightwards.
+		self.ConditionList_radii = cl_radii
+		self.ConditionList_YawRate_offsets = cl_yawrates
+
+		##### ADD GRASS TEXTURE #####
+		gplane1 = setStage()
+		self.gplane1 = gplane1		
+
+		##### MAKE BEND OBJECTS #####
+		[leftbends,rightbends] = BendMaker(self.FACTOR_radiiPool)
+		self.leftbends = leftbends
+		self.rightbends = rightbends 
+
+		self.callback(viz.TIMER_EVENT,self.updatePositionLabel)
+		self.starttimer(0,0,viz.FOREVER) #self.update position label is called every frame.
+		
+		####### DATA SAVING ######
+		datacolumns = ['ppid', 'radius','yawrate_offset','trialn','timestamp','trialtype_signed','World_x','World_z','WorldYaw','SWA','YawRate_seconds','TurnAngle_frames','Distance_frames','dt']
+		self.Output = pd.DataFrame(columns=datacolumns) #make new empty EndofTrial data
+
+		### parameters that are set at the start of each trial ####
+		self.Trial_radius = 0
+		self.Trial_YawRate_Offset = 0 				
+		self.Trial_N = 0
+		self.Trial_trialtype_signed = 0			
+		self.Trial_Timer = 0 #keeps track of trial length. 
+		self.Trial_BendObject = None		
+		
+		#### parameters that are updated each timestamp ####
+		self.Current_pos_x = 0
+		self.Current_pos_z = 0
+		self.Current_yaw = 0
+		self.Current_SWA = 0
+		self.Current_Time = 0
+		self.Current_RowIndex = 0
+		self.Current_YawRate_seconds = 0
+		self.Current_TurnAngle_frames = 0
+		self.Current_distance = 0
+		self.Current_dt = 0
+
+		self.callback(viz.EXIT_EVENT,self.SaveData) #if exited, save the data. 
+
+	def runtrials(self):
+		"""Loops through the trial sequence"""
+		
+		if self.EYETRACKING:
+			filename = str(self.EXP_ID) + "_Calibration" #+ str(demographics[0]) + "_" + str(demographics[2]) #add experimental block to filename
+			print (filename)
+			yield run_calibration(comms, filename)
+			yield run_accuracy(comms, filename)		
+
+		self.driver = vizdriver.Driver(self.caveview)	
+
+		
+		viz.MainScene.visible(viz.ON,viz.WORLD)		
+	
+		#add text to denote conditons.
+		txtCondt = viz.addText("Condition",parent = viz.SCREEN)
+		txtCondt.setPosition(.7,.2)
+		txtCondt.fontSize(36)		
+
+		if self.EYETRACKING:
+			comms.start_trial()
+		
+		for i, trialtype_signed in enumerate(self.TRIALSEQ_signed):
+			#import vizjoy		
+			print("Trial: ", str(i))
+			print("TrialType: ", str(trialtype_signed))
+			
+			trialtype = abs(trialtype_signed)
+
+			trial_radii = self.ConditionList_radii[trialtype] #set radii for that trial
+			trial_yawrate_offset = self.ConditionList_YawRate_offsets[trialtype] #set target number for the trial.
+
+			print(str([trial_radii, trial_yawrate_offset]))
+
+			txtDir = ""
+			
+			print ("Length of bend array:", len(self.rightbends))
+
+			radius_index = self.FACTOR_radiiPool.index(trial_radii)
+
+			#choose correct road object.
+			if trialtype_signed > 0: #right bend
+				trialbend = self.rightbends[radius_index]
+				txtDir = "R"
+			else:
+				trialbend = self.leftbends[radius_index]
+				txtDir = "L"
+						
+			if trial_radii > 0: #if trial_radii is above zero it is a bend, not a straight 
+				msg = "Radius: " + str(trial_radii) + txtDir + '_' + str(trial_yawrate_offset)
+			else:
+				msg = "Radius: Straight" + txtDir + '_' + str(trial_yawrate_offset)
+			txtCondt.message(msg)	
+
+			#update class#
+			self.Trial_N = i
+			self.Trial_radius = trial_radii
+			self.Trial_YawRate_Offset = trial_yawrate_offset			
+			self.Trial_BendObject = trialbend			
+
+			yield viztask.waitTime(self.TrialLength) #wait for input .		
+	
+		#loop has finished.
+		CloseConnections(self.EYETRACKING)
+		#viz.quit() 
+
+	def getNormalisedEuler(self):
+		"""returns three dimensional euler on 0-360 scale"""
+		
+		euler = self.caveview.getEuler()
+		
+		euler[0] = vizmat.NormAngle(euler[0])
+		euler[1] = vizmat.NormAngle(euler[1])
+		euler[2] = vizmat.NormAngle(euler[2])
+
+		return euler	
+
+
+	def RecordData(self):
+		
+		"""Records Data into Dataframe"""
+
+		#datacolumns = ['ppid', 'radius','occlusion','trialn','timestamp','trialtype_signed','World_x','World_z','WorldYaw','SWA']
+		output = [self.PP_id, self.Trial_radius, self.Trial_YawRate_Offset, self.Trial_N, self.Current_Time, self.Trial_trialtype_signed, 
+		self.Current_pos_x, self.Current_pos_z, self.Current_yaw, self.Current_SWA, self.Current_YawRate_seconds, self.Current_TurnAngle_frames, 
+		self.Current_distance, self.Current_dt] #output array.
+		
+		self.Output.loc[self.Current_RowIndex,:] = output #this dataframe is actually just one line. 		
+	
+	def SaveData(self):
+
+		"""Saves Current Dataframe to csv file"""
+
+		fname = 'Data//Midline_' + self.Trial_radius + '_' + self.Trial_N + '.csv'
+		self.Output.to_csv(fname)
+
+	def updatePositionLabel(self, num):
+		
+		"""Timer function that gets called every frame. Updates parameters for saving"""
+
+		"""Here need to bring in steering bias updating from Trout as well"""
+
+		#print("UpdatingPosition...")	
+		#update driver view.
+		UpdateValues = self.driver.UpdateView() #update view and return values used for update
+		
+		# get head position(x, y, z)
+		pos = self.caveview.getPosition()				
+		ori = self.getNormalisedEuler()	
+									
+		### #update Current parameters ####
+		self.Current_pos_x = pos[0]
+		self.Current_pos_z = pos[2]
+		self.Current_SWA = UpdateValues[4]
+		self.Current_yaw = ori
+		self.Current_RowIndex += 1
+		self.Current_Time = viz.tick()
+		self.Current_YawRate_seconds = UpdateValues[0]
+		self.Current_TurnAngle_frames = UpdateValues[1]
+		self.Current_distance = UpdateValues[2]
+		self.Current_dt = UpdateValues[3]
+
+
+		self.RecordData() #write a line in the dataframe.	
+
+def CloseConnections(EYETRACKING):
+	
+	"""Shuts down EYETRACKING and wheel threads then quits viz"""		
+	
+	print ("Closing connections")
+	if EYETRACKING: 
+	 	comms.stop_trial() #closes recording			
+	
+	#kill automation
+	viz.quit()
+	
+if __name__ == '__main__':
+
+	###### SET EXPERIMENT OPTIONS ######	
+	EYETRACKING = True
+	PRACTICE = True
+	TILING = False
+	EXP_ID = "Orca18"
+
+	if PRACTICE == True: # HACK
+		EYETRACKING = False 
+
+	myExp = myExperiment(EYETRACKING, PRACTICE, EXP_ID)
+
+	viz.callback(viz.EXIT_EVENT,CloseConnections, myExp.EYETRACKING)
+
+	viztask.schedule(myExp.runtrials())
+
