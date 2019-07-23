f6a94389be442d58e287755414788ec168ec5a05
callummole <c.d.mole@leeds.ac.uk>
2019-07-23 09:33:00 +0100

    change port to match pupil_remote

--- pupil_logger.py
+++ pupil_logger.py
@@ -7,17 +7,25 @@ import threading
 def start_logging(logfile, host="192.168.0.2", port=50020, timestamper=time.time):
     ctx = zmq.Context()
 
+    """
+    the port needs to match the port on pupil_remote. 
+    
+    The venlab_remote plugin script also interfaces with pupil_remote, and uses port 50020. 
+        
+    """
+
     # We need to first request the port for the subscriber.
     # A bit of a hassle, but should be quite clear.
-    req = ctx.socket(zmq.REQ)
-    req.connect("tcp://%s:%i"%(host, port))
-    req.send(b'SUB_PORT')
-    sub_port = int(req.recv())
+    req = ctx.socket(zmq.REQ)    
+    req.connect("tcp://%s:%i"%(host, port))    
+    req.send(b'SUB_PORT')    
+    sub_port = int(req.recv())    
     req.close()
 
     sub = ctx.socket(zmq.SUB)
     sub.connect("tcp://%s:%i"%(host, sub_port))
     sub.setsockopt(zmq.SUBSCRIBE, '')
+    
     def do_log():
         while True:
             try:
@@ -36,6 +44,7 @@ def start_logging(logfile, host="192.168.0.2", port=50020, timestamper=time.time
         ctx.term()
         thread.join()
     thread.start()
+    
     return stop_logging
 
 if __name__ == '__main__':
